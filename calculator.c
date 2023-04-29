460965da04d0e0bc861b15ecf6cea90472d4638d
A06uv <130320700+A06uv@users.noreply.github.com>
2023-04-29 14:29:36 +0000

    ddd, Apr 4/29/2023, YYYY, 2:29 A +0

--- calculator.c
+++ calculator.c
@@ -4,9 +4,55 @@
 
 int main(void)
 {
-// Accept Input From user (char), where (a) stands for "Addition", (m) for "Multiplications," and (d) for "Division,"
-  
+#include <iostream>
+#include "cs50.h"
 
-  
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    char op;
+
+    cout << "Enter first number: ";
+    num1 = get_int();
+
+    cout << "Enter second number: ";
+    num2 = get_int();
+
+    cout << "Enter operation (+, -, *, /): ";
+    op = get_char();
+
+    switch(op)
+    {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+            break;
+        case '/':
+            if(num2 == 0)
+            {
+                cout << "Error: Cannot divide by zero." << endl;
+            }
+            else
+            {
+                cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+            }
+            break;
+        default:
+            cout << "Error: Invalid operation." << endl;
+            break;
+    }
+
+    return 0;
+}
+
+
+
 
 }
--- hello.c
+++ hello.c
@@ -1,12 +1,4 @@
 #include <stdio.h>
 
 int main(void)
-{
-  
-  // Step ( 1 ) : Print Hello, World
-  
-  
-  
-  //Step ( 2 ) : Print Hello, Your Name
-
-}
+ {prinf("hello, MY NAME IS A");}
